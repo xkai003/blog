60de6807b07ac327305bf6f3725118d4e2db1ed3
xkai003 <xkai03@outlook.com>
2026-02-13 15:42:14 +0800

    优化导航

--- js/Side_navigation_bar.js
+++ js/Side_navigation_bar.js
@@ -20,6 +20,27 @@ document.getElementById('sidebarToggle').addEventListener('click', function() {
     }
 });
 
+document.getElementById('btn').onclick = function() {
+            const townav = document.getElementById('twonav');
+            const jt = document.getElementById('jt');
+        
+            // 关键：用 getComputedStyle 获取实际生效的样式（不管是行内还是CSS表）
+            const currentDisplay = window.getComputedStyle(townav).display;
+    
+            if (currentDisplay === "none") {
+                // 显示元素
+                townav.style.display = "block";
+                townav.style.transform = "translate(0%, 0%)"
+                // 给箭头添加动画效果
+                jt.style.transform = "rotate(90deg)";
+            } else {
+                // 隐藏元素
+                townav.style.display = "none";
+                // 给箭头添加动画效果
+                jt.style.transform = "rotate(0deg)";
+            }
+        };
+
 // 
 // 一进页面就执行
 // window.onload = function(){
